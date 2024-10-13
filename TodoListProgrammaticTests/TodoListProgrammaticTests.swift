f93498510811de7f3522c5d9ecd8ca567e2bf20a
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2024-10-13 15:31:51 +0700

    Initial Commit

---
+++ TodoListProgrammaticTests/TodoListProgrammaticTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListProgrammaticTests.swift
+//  TodoListProgrammaticTests
+//
+//  Created by MacBook Difta on 13/10/24.
+//
+
+import Testing
+@testable import TodoListProgrammatic
+
+struct TodoListProgrammaticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
